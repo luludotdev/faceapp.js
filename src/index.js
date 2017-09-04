b2e16124a2ffd2b15a77471eef13fb552071136e
Jack Baron <yodude107@gmail.com>
2017-09-05 00:28:22 +0100

    Get Filtered Image

--- src/index.js
+++ src/index.js
@@ -14,34 +14,66 @@ const superagent = require('superagent')
 /**
  * @typedef {Object} AvailableFilters
  * @property {string} code
+ * @property {string} deviceID
  * @property {Filter[]} filters
  */
 
 /**
  * @param {string} path Input File
- * @returns {AvailableFilters}
+ * @returns {Promise.<AvailableFilters>}
  */
 const getAvailableFilters = async path => {
-  let DEVICE_ID = constants.generateDeviceID()
+  let deviceID = constants.generateDeviceID()
   try {
     let res = await superagent.post(`${constants.API_BASE_URL}/api/v2.6/photos`)
       .set('User-Agent', constants.API_USER_AGENT)
-      .set('X-FaceApp-DeviceID', DEVICE_ID)
+      .set('X-FaceApp-DeviceID', deviceID)
       .attach('file', path)
 
     let code = res.body.code
     let filters = res.body.filters
-      .filter(o => o.is_paid === false)
+      .filter(o => o.is_paid === false && o.group === '0')
       .map(o => ({
         id: o.id,
         title: o.title,
         cropped: o.only_cropped,
       }))
 
-    return { code, filters }
+    return { code, deviceID, filters }
   } catch (err) {
     throw err
   }
 }
 
-module.exports = { getAvailableFilters }
+/**
+ * 
+ * @param {AvailableFilters} args Input Object
+ * @param {string} filterID Filter ID
+ * @returns {Promise.<Buffer>}
+ */
+const getFilterImage = async (args, filterID = 'no-filter') => {
+  let filterArr = args.filters.filter(o => o.id === filterID)
+  if (filterArr.length === 0) {
+    let filters = args.filters.map(o => o.id).join(', ')
+    throw new Error(`Invalid Filter ID\nAvailable Filters: '${filters}'`)
+  }
+
+  let filter = filterArr[0]
+  let cropped = filter.cropped ? '1' : '0'
+  let url = `${constants.API_BASE_URL}/api/v2.6/photos/${args.code}/filters/${filter.id}?cropped=${cropped}`
+
+  try {
+    let res = await superagent.get(url)
+      .set('User-Agent', constants.API_USER_AGENT)
+      .set('X-FaceApp-DeviceID', args.deviceID)
+
+    return res.body
+  } catch (err) {
+    throw err
+  }
+}
+
+module.exports = {
+  getAvailableFilters,
+  getFilterImage,
+}
