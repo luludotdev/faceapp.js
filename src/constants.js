5e32869d720b5e234d7c6b77cbdf40cfcbd0ab73
Jack Baron <yodude107@gmail.com>
2017-09-08 03:11:05 +0100

    Update Version Number in UA

--- src/constants.js
+++ src/constants.js
@@ -9,6 +9,6 @@ const generateDeviceID = () => randomstring.generate(16)
 
 module.exports = {
   API_BASE_URL: 'https://node-03.faceapp.io',
-  API_USER_AGENT: 'FaceApp/2.0.553 (Linux; Android 6.0)',
+  API_USER_AGENT: 'FaceApp/2.0.561 (Linux; Android 6.0)',
   generateDeviceID,
 }
