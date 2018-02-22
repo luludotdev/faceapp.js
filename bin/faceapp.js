007827cb0d47cd23c0cfa69edcd13cd8ed6a352e
Jack Baron <jackmbaron@gmail.com>
2018-02-22 19:46:45 +0000

    Refactor to seperate functions

--- bin/faceapp.js
+++ bin/faceapp.js
@@ -8,25 +8,36 @@ const faceapp = require('../src/index')
 
 program
   .version(require('../package.json').version)
-  .option('-l, --list-filters', 'Lists all filters you can use')
+  .option('-o, --output', 'filepath to output to')
+  .option('-l, --list-filters', 'lists all filters you can use')
   .parse(process.argv)
 
 /**
  * @param {program} p Commander Program
  */
-const main = async p => {
+const main = p => {
   if (p.args.length === 0 && !p.listFilters) p.help()
-  if (p.listFilters) {
-    let filters = (await faceapp.listFilters()).filter(x => x.id !== 'no-filter')
-    let free = filters.filter(x => !x.paid)
-    let paid = filters.filter(x => x.paid)
+  if (p.listFilters) listFilters()
+  else run(p)
+}
 
-    console.log(chalk`{blue ${'Free Filters:'}}
+const listFilters = async () => {
+  let filters = (await faceapp.listFilters()).filter(x => x.id !== 'no-filter')
+  let free = filters.filter(x => !x.paid)
+  let paid = filters.filter(x => x.paid)
+
+  console.log(chalk`{blue ${'Free Filters:'}}
 ${free.map(x => x.id).join(' ')}
 
 {blue ${'Paid Filters:'}}
 ${paid.map(x => x.id).join(' ')}`)
-  }
+}
+
+/**
+ * @param {program} p Commander Program
+ */
+const run = async p => {
+  console.log('yes')
 }
 
 main(program)
