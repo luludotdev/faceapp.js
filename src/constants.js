2b3f5d1a2db33322fdac73f695502d43a5b519c2
Essem <smswessem@gmail.com>
2018-11-20 12:18:24 -0600

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -10,6 +10,6 @@ const generateDeviceID = () => randomstring.generate(16)
 module.exports = {
   TEST_IMAGE_URL: 'https://i.imgur.com/nVsxMNp.jpg',
   API_BASE_URL: 'https://node-03.faceapp.io',
-  API_USER_AGENT: 'FaceApp/2.0.561 (Linux; Android 6.0)',
+  API_USER_AGENT: 'FaceApp/3.2.2 (Linux; Android 8.1)',
   generateDeviceID,
 }
